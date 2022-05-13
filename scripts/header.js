214e55f226875d2b358033451eb2508c3cd380c6
VeryDarkRaven <48388485+VeryDarkRaven@users.noreply.github.com>
2022-05-13 16:45:18 +0300

    uploading layout files

---
+++ scripts/header.js
@@ -0,0 +1,52 @@
+// Нажатие на ссылку шапки 
+function navLinksClick () {
+    const headerNavs = document.getElementsByClassName("nav");
+    const burgerIcon = document.querySelector(".header__burger");
+
+    const burgerClassList = burgerIcon.classList;
+    let navLinks;
+
+    for (let burgerClass of burgerClassList) {
+        if (burgerClass === "header__burger_active") {
+            navLinks = headerNavs[1].getElementsByTagName("a");
+        } else {
+            navLinks = headerNavs[0].getElementsByTagName("a");
+        }
+    }
+
+    for (let link of navLinks) {
+        if (link.classList === "sign-in") {
+            continue;
+        };
+
+        link.onclick = () => {
+            link.classList.add("nav__active-link");
+
+            for (let activeLink of navLinks) {
+                if (activeLink !== link) {
+                    activeLink.classList.remove("nav__active-link");
+                }
+            }
+        }
+
+    }
+}
+
+navLinksClick();
+
+
+
+// Нажатие на бургер 
+function burgerClick () {
+    const burgerIcon = document.querySelector(".header__burger");
+    const dropdown = document.querySelector(".dropdown");
+
+    burgerIcon.onclick = () => {
+        burgerIcon.classList.toggle("header__burger_active");
+        dropdown.classList.toggle("dropdown_active");
+
+        navLinksClick();
+    };
+}
+
+burgerClick();
